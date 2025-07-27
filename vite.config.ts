dfd8f543608f78a15cb1d862e528e026960d5643
Đặng Quốc Tâm <tamdqse171713@fpt.edu.vn>
2025-07-28 02:30:39 +0700

    x1

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/SuckhoeToanDien/', // Đặt base path cho ứng dụng
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
